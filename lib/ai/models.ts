b58eb1c38cabf00ece9217e0a15c870815dd5f91
Paul <2900717209@qq.com>
2025-02-27 16:00:10 +0800

    modify ao models

--- lib/ai/models.ts
+++ lib/ai/models.ts
@@ -1,50 +1,52 @@
-import { openai } from '@ai-sdk/openai';
-import { fireworks } from '@ai-sdk/fireworks';
+import { openai } from "@ai-sdk/openai"
+import { fireworks } from "@ai-sdk/fireworks"
 import {
   customProvider,
   extractReasoningMiddleware,
   wrapLanguageModel,
-} from 'ai';
+} from "ai"
 
-export const DEFAULT_CHAT_MODEL: string = 'chat-model-small';
+export const DEFAULT_CHAT_MODEL: string = "chat-model-small"
 
 export const myProvider = customProvider({
   languageModels: {
-    'chat-model-small': openai('gpt-4o-mini'),
-    'chat-model-large': openai('gpt-4o'),
-    'chat-model-reasoning': wrapLanguageModel({
-      model: fireworks('accounts/fireworks/models/deepseek-r1'),
-      middleware: extractReasoningMiddleware({ tagName: 'think' }),
+    "chat-model-small": openai("gpt-3.5-turbo"),
+    // 'chat-model-small': openai('gpt-4o-mini'),
+    "chat-model-large": openai("o3-mini"),
+    // "chat-model-large": openai("gpt-4o"),
+    "chat-model-reasoning": wrapLanguageModel({
+      model: fireworks("accounts/fireworks/models/deepseek-r1"),
+      middleware: extractReasoningMiddleware({ tagName: "think" }),
     }),
-    'title-model': openai('gpt-4-turbo'),
-    'artifact-model': openai('gpt-4o-mini'),
+    "title-model": openai("gpt-4-turbo"),
+    "artifact-model": openai("gpt-4o-mini"),
   },
   imageModels: {
-    'small-model': openai.image('dall-e-2'),
-    'large-model': openai.image('dall-e-3'),
+    "small-model": openai.image("dall-e-2"),
+    "large-model": openai.image("dall-e-3"),
   },
-});
+})
 
 interface ChatModel {
-  id: string;
-  name: string;
-  description: string;
+  id: string
+  name: string
+  description: string
 }
 
 export const chatModels: Array<ChatModel> = [
   {
-    id: 'chat-model-small',
-    name: 'Small model',
-    description: 'Small model for fast, lightweight tasks',
+    id: "chat-model-small",
+    name: "Small model",
+    description: "Small model for fast, lightweight tasks",
   },
   {
-    id: 'chat-model-large',
-    name: 'Large model',
-    description: 'Large model for complex, multi-step tasks',
+    id: "chat-model-large",
+    name: "Large model",
+    description: "Large model for complex, multi-step tasks",
   },
   {
-    id: 'chat-model-reasoning',
-    name: 'Reasoning model',
-    description: 'Uses advanced reasoning',
+    id: "chat-model-reasoning",
+    name: "Reasoning model",
+    description: "Uses advanced reasoning",
   },
-];
+]
